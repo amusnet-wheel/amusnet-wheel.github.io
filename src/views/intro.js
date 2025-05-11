4aa6f2b19a906c5511b90e13533d19d4fbc21ef8
Viktor Kostadinov <victor.kostadinov@amusnet.com>
2025-05-12 01:02:20 +0300

    add settings link and full screen to intro screen

--- src/views/intro.js
+++ src/views/intro.js
@@ -2,18 +2,29 @@ import { html } from '@lit';
 import page from '@page';
 
 const goToMainPage = () => {
+    if (!document.fullscreenElement) {
+        if (typeof document.documentElement.webkitRequestFullscreen == 'function') {
+            // safari
+            document.documentElement.webkitRequestFullscreen();
+        } else {
+            document.documentElement.requestFullscreen();
+        }
+    }
+
     page.redirect('/wheel');
 };
 
 const introView = () => html`
 <div class="start-page">
-   <div class="logo"></div>
-      <button class="start-app" @click=${goToMainPage}>Start app</button>
-      </div>
-  `;
+    <div class="logo"></div>
+    <button class="start-app" @click=${goToMainPage}>Start app</button>
+    <div class="start-app">
+        <a href="/settings">Settings</a>
+    </div>
+</div>
+`;
 
 /** @type {import('../index').ViewController} */
 export function showIntro(ctx) {
-    // sessionStorage.setItem('refresh-handled', 'false');
     ctx.render(introView());
 }
